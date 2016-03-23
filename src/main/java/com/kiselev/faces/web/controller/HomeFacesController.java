2797c721e66345130b19e6a12a190610fd2246ae
Vadim Kiselev <vadim8kiselev@gmail.com>
2016-03-23 13:28:33 -0800

    Improve sign (in / up) controllers (without error messages)

--- src/main/java/com/kiselev/faces/web/controller/HomeFacesController.java
+++ src/main/java/com/kiselev/faces/web/controller/HomeFacesController.java
@@ -1,16 +1,27 @@
 package com.kiselev.faces.web.controller;
 
+import com.kiselev.faces.common.component.SessionComponent;
+import com.kiselev.faces.common.dao.impl.ProfileDAO;
 import com.kiselev.faces.common.entities.ProfileEntity;
+import com.kiselev.faces.web.validator.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeFacesController {
+
+    @Autowired
+    ProfileDAO dao;
+
+    @Autowired
+    SessionComponent component;
+
+    @Autowired
+    Validator validator;
 
     @RequestMapping(value = {"/", "/signup"}, method = RequestMethod.GET)
     public String getHandler(Model model) {
@@ -19,8 +30,29 @@ public class HomeFacesController {
     }
 
     @RequestMapping(value = "/method/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("profile") ProfileEntity profile,
-                         BindingResult result, ModelMap model) {
-        return "redirect:/register";
+    public String signup(@ModelAttribute("profile") ProfileEntity profile) {
+
+        if (validator.fieldIsNotEmpty(profile.getUsername()) &&
+                validator.fieldIsNotEmpty(profile.getPassword())) {
+
+            profile.secret();
+            if (!dao.existUsername(profile.getUsername())) {
+                Long id = dao.saveProfile(profile);
+
+                if (id != null) {
+                    component.setId(id);
+                    return "redirect:/register";
+                } else {
+                    //Incorrect username or password
+                    return "redirect:/signup";
+                }
+            } else {
+                //This nickname is already taken
+                return "redirect:/signup";
+            }
+        } else {
+            //This fields cannot be blank
+            return "redirect:/signup";
+        }
     }
 }
--- src/main/java/com/kiselev/faces/web/controller/SignInFacesController.java
+++ src/main/java/com/kiselev/faces/web/controller/SignInFacesController.java
@@ -1,16 +1,27 @@
 package com.kiselev.faces.web.controller;
 
+import com.kiselev.faces.common.component.SessionComponent;
+import com.kiselev.faces.common.dao.impl.ProfileDAO;
 import com.kiselev.faces.common.entities.ProfileEntity;
+import com.kiselev.faces.web.validator.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SignInFacesController {
+
+    @Autowired
+    ProfileDAO dao;
+
+    @Autowired
+    SessionComponent component;
+
+    @Autowired
+    Validator validator;
 
     @RequestMapping(value = "/signin", method = RequestMethod.GET)
     public String getHandler(Model model) {
@@ -19,8 +30,24 @@ public class SignInFacesController {
     }
 
     @RequestMapping(value = "/method/signin", method = RequestMethod.POST)
-    public String signin(@ModelAttribute("profile") ProfileEntity profile,
-                         BindingResult result, ModelMap model) {
-        return "redirect:/profile";
+    public String signin(@ModelAttribute("profile") ProfileEntity profile) {
+
+        if (validator.fieldIsNotEmpty(profile.getUsername()) &&
+                validator.fieldIsNotEmpty(profile.getPassword())) {
+
+            profile.secret();
+            Long id = dao.getId(profile);
+
+            if (id != null) {
+                component.setId(id);
+                return "redirect:/id" + id;
+            } else {
+                //Incorrect username or password
+                return "redirect:/signin";
+            }
+        } else {
+            //This fields cannot be blank
+            return "redirect:/signin";
+        }
     }
 }
