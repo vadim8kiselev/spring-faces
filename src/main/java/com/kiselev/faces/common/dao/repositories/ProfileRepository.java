4e4c9ed3852aae4ce51a405d6fd982631014caa2
Vadim Kiselev <vadim8kiselev@gmail.com>
2016-03-23 02:43:25 -0800

    Rewrite getId method

--- src/main/java/com/kiselev/faces/common/dao/repositories/ProfileRepository.java
+++ src/main/java/com/kiselev/faces/common/dao/repositories/ProfileRepository.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProfileRepository extends CrudRepository<ProfileEntity, Long> {
 
-    @Query("SELECT p.id FROM ProfileEntity p WHERE p.username = :username AND p.password = :password")
-    Long getId(@Param("username") String username, @Param("password") String password);
+    @Query("SELECT p.id FROM ProfileEntity p WHERE p.username = :username")
+    Long getId(@Param("username") String username);
 
     @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM ProfileEntity p WHERE p.urlName = :urlName")
     boolean exists(@Param("urlName") String urlName);
