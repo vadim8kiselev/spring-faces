b7f43e5d5bbe8991688e33bd1db77caf1943c264
Vadim Kiselev <vadim8kiselev@gmail.com>
2016-03-23 13:29:56 -0800

    Fix method name in ProfileController (still initial)

--- src/main/java/com/kiselev/faces/web/controller/ProfileFacesController.java
+++ src/main/java/com/kiselev/faces/web/controller/ProfileFacesController.java
@@ -1,5 +1,8 @@
 package com.kiselev.faces.web.controller;
 
+import com.kiselev.faces.common.component.SessionComponent;
+import com.kiselev.faces.common.dao.impl.ProfileDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ProfileFacesController {
 
+    @Autowired
+    ProfileDAO dao;
+
+    @Autowired
+    SessionComponent component;
+
     @RequestMapping(value = "/id{id}", method = RequestMethod.GET)
     public String getHandler(@PathVariable Long id) {
         return "profile";
     }
 
     @RequestMapping(value = "/{username}", method = RequestMethod.GET)
-    public String welcomeHandler(@PathVariable String username) {
+    public String getHandler(@PathVariable String username) {
         return "profile";
     }
 }
