409f2ec831ca9bce6d1390e2cf9dc082f8d428f2
Vadim Kiselev <vadim8kiselev@gmail.com>
2016-03-23 13:34:58 -0800

    Fix method name in settings controller (still initial)

--- src/main/java/com/kiselev/faces/web/controller/SettingsFacesController.java
+++ src/main/java/com/kiselev/faces/web/controller/SettingsFacesController.java
@@ -1,5 +1,8 @@
 package com.kiselev.faces.web.controller;
 
+import com.kiselev.faces.common.dao.impl.ProfileDAO;
+import com.kiselev.faces.web.validator.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,13 +10,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class SettingsFacesController {
 
+    @Autowired
+    ProfileDAO dao;
+
+    @Autowired
+    Validator validator;
+
     @RequestMapping(value = "/settings", method = RequestMethod.GET)
     public String getHandler() {
         return "settings";
     }
 
     @RequestMapping(value = "/method/settings", method = RequestMethod.POST)
-    public String register() {
-        return "profile";
+    public String settings() {
+        return "redirect:/profile";
     }
 }
