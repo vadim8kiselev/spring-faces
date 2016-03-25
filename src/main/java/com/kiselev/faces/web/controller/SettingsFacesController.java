3b85cdd0fd2f4158daf2ceccd8763323d40a5d62
Vadim Kiselev <vadim8kiselev@gmail.com>
2016-03-24 19:56:36 -0800

    Complete settings controller

--- src/main/java/com/kiselev/faces/web/controller/SettingsFacesController.java
+++ src/main/java/com/kiselev/faces/web/controller/SettingsFacesController.java
@@ -1,11 +1,17 @@
 package com.kiselev.faces.web.controller;
 
+import com.kiselev.faces.common.component.SessionComponent;
 import com.kiselev.faces.common.dao.impl.ProfileDAO;
+import com.kiselev.faces.common.entities.ProfileEntity;
 import com.kiselev.faces.web.validator.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class SettingsFacesController {
@@ -14,15 +20,45 @@ public class SettingsFacesController {
     ProfileDAO dao;
 
     @Autowired
+    SessionComponent component;
+
+    @Autowired
     Validator validator;
 
     @RequestMapping(value = "/settings", method = RequestMethod.GET)
-    public String getHandler() {
+    public String getHandler(ModelMap model) {
+        model.addAttribute("profile", dao.getProfile(component.getId()));
         return "settings";
     }
 
     @RequestMapping(value = "/method/settings", method = RequestMethod.POST)
-    public String settings() {
-        return "redirect:/profile";
+    public String settings(@ModelAttribute("profile") ProfileEntity profile) {
+
+        ProfileEntity old = dao.getProfile(component.getId());
+        profile.setId(old.getId());
+        profile.setUsername(old.getUsername());
+        profile.setPassword(old.getPassword());
+
+        if (dao.saveProfile(profile) != null) {
+
+            if (profile.getUrlName() != null && !profile.getUrlName().equals(component.getUrlName())) {
+                component.setUrlName(profile.getUrlName());
+            }
+
+            if (component.getUrlName() != null) {
+                return "redirect:/" + component.getUrlName();
+            } else {
+                return "redirect:/id" + component.getId();
+            }
+        } else {
+            return "redirect:/settings";
+        }
+    }
+
+    @RequestMapping(value = "/method/delete", method = RequestMethod.POST)
+    public String delete(HttpSession session) {
+        dao.deleteProfile(component.getId());
+        session.invalidate();
+        return "redirect:/signup";
     }
 }
