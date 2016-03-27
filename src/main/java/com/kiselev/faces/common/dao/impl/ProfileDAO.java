1096eb3f4e59aedb93f774f2d93406bac71e631a
Vadim Kiselev <vadim8kiselev@gmail.com>
2016-03-26 19:35:20 -0800

    Implement logging in DAO

--- src/main/java/com/kiselev/faces/common/dao/impl/ProfileDAO.java
+++ src/main/java/com/kiselev/faces/common/dao/impl/ProfileDAO.java
@@ -3,6 +3,7 @@ package com.kiselev.faces.common.dao.impl;
 import com.kiselev.faces.common.dao.DAO;
 import com.kiselev.faces.common.dao.repositories.ProfileRepository;
 import com.kiselev.faces.common.entities.ProfileEntity;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -11,20 +12,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProfileDAO implements DAO {
 
+    final static Logger logger = Logger.getLogger(ProfileDAO.class);
+
     @Autowired
     ProfileRepository profileRepository;
 
     public Long getId(ProfileEntity profile) {
         if (profile != null) {
             return profileRepository.getId(profile.getUsername(), profile.getPassword());
-        } else
+        } else {
             return null;
+        }
     }
 
     public Long saveProfile(ProfileEntity profile) {
         try {
             return profileRepository.save(profile).getId();
         } catch (DataIntegrityViolationException e) {
+            logger.error("Profile is not saved", e);
             return null;
         }
     }
@@ -34,6 +39,7 @@ public class ProfileDAO implements DAO {
             profileRepository.delete(id);
             return id;
         } catch (EmptyResultDataAccessException e) {
+            logger.error("Profile is not deleted", e);
             return null;
         }
     }
